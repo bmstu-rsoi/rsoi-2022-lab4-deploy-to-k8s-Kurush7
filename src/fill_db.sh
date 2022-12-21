ee230f849f92f09a971b56ceb7c925b1963b62c1
Kurush7 <ze17@yandex.ru>
2022-12-21 14:33:37 +0300

    wip

--- src/fill_db.sh
+++ src/fill_db.sh
@@ -1,8 +1,14 @@
-docker ps -a
-docker exec -i postgres psql -U program libraries < test/libraries.dump
-docker exec -i postgres psql -U program ratings < test/ratings.dump
-docker exec -i postgres psql -U program reservations < test/reservations.dump
+postgresPod=$1
 
-docker exec -i postgres psql -U program libraries -c "\dt"
-docker exec -i postgres psql -U program ratings -c "\dt"
-docker exec -i postgres psql -U program reservations -c "\dt"
+cat test/libraries.dump           | kubectl exec -i $postgresPod -- psql -U program libraries
+cat test/ratings.dump        | kubectl exec -i $postgresPod -- psql -U program ratings
+cat test/reservations.dump           | kubectl exec -i $postgresPod -- psql -U program reservations
+
+#docker ps -a
+#docker exec -i postgres psql -U program libraries < test/libraries.dump
+#docker exec -i postgres psql -U program ratings < test/ratings.dump
+#docker exec -i postgres psql -U program reservations < test/reservations.dump
+#
+#docker exec -i postgres psql -U program libraries -c "\dt"
+#docker exec -i postgres psql -U program ratings -c "\dt"
+#docker exec -i postgres psql -U program reservations -c "\dt"
